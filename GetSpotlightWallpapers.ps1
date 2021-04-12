bf011ff1a0122064f2e679cb0e9c90e24a93b6d7
Seabagel <hsrt1367tim@gmail.com>
2021-04-12 11:17:45 -0700

    Prettify code with comments

--- GetSpotlightWallpapers.ps1
+++ GetSpotlightWallpapers.ps1
@@ -1,12 +1,10 @@
+# Destination and Source Paths
 $DesktopPath = "C:\Users\" + $env:UserName + "\Desktop\Wallpapers\"
 $WallpaperPath = "C:\Users\" + $env:UserName + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 
+# Test if Path is valid, then make the directory
 If( !(Test-Path $DesktopPath) ){ New-Item -ItemType Directory -Force -Path $DesktopPath }
 If( !(Test-Path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
 
-cd $DesktopPath
+# Copy each file in Wallpaper folder, copy it to the Desktop folder, and name add an extension
 gci $WallpaperPath -File | Copy-Item -Force -Destination {$DesktopPath + $_.BaseName + ".png"}
- 
-# ? { (!Test-Path ($DesktopPath + $BaseName + ".png") }
-# gci $WallpaperPath -File | ? {(-not (Test-Path ($DesktopPath + $BaseName + ".png"))) {Copy-Item -Destination $DesktopPath}}
-# gci $DesktopPath -File | Rename-Item -NewName { $_.BaseName + ".png"}
