a42d68c9984389c9421959d2cad48927e4b39c69
Seabagel <hsrt1367tim@gmail.com>
2021-04-12 10:50:22 -0700

    Add Duplicate check

--- GetSpotlightWallpapers.ps1
+++ GetSpotlightWallpapers.ps1
@@ -1,9 +1,9 @@
 $DesktopPath = "C:\Users\" + $env:UserName + "\Desktop\Wallpapers"
 $WallpaperPath = "C:\Users\" + $env:UserName + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 
-If( !(test-path $DesktopPath) ){ New-Item -ItemType Directory -Force -Path $DesktopPath }
-If( !(test-path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
+If( !(Test-Path $DesktopPath) ){ New-Item -ItemType Directory -Force -Path $DesktopPath }
+If( !(Test-Path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
 
 cd $DesktopPath
-gci $WallpaperPath -File | Copy-Item -Destination $DesktopPath -Force
-gci $DesktopPath -File | Rename-Item -NewName { $_.basename + ".png"}
+gci $WallpaperPath -File | If(!(Test-Path ($DesktopPath + $BaseName + ".png"))) Copy-Item -Destination $DesktopPath
+gci $DesktopPath -File | Rename-Item -NewName { $_.BaseName + ".png"}
