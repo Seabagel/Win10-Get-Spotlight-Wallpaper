2b9f1ead28be4d748a2b534d12f5f53237e06865
Seabagel <hsrt1367tim@gmail.com>
2021-04-12 10:37:29 -0700

    remove mkdir

--- GetSpotlightWallpapers.ps1
+++ GetSpotlightWallpapers.ps1
@@ -2,9 +2,9 @@ $DesktopPath = "C:\Users\" + $env:UserName + "\Desktop\Wallpapers"
 $WallpaperPath = "C:\Users\" + $env:UserName + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 
 If( !(test-path $DesktopPath) ){ New-Item -ItemType Directory -Force -Path $DesktopPath }
-If( !(test-path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
-
 cd $DesktopPath
-mkdir $WallpaperPath
+
+If( !(test-path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
 xcopy $WallpaperPath $DesktopPath /i /q /y
+
 gci -File | Rename-Item -NewName { $_.basename + ".png"}
