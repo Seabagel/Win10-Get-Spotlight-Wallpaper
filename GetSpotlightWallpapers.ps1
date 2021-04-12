948de482815e6a6948062332484b8b10836d2bae
Seabagel <hsrt1367tim@gmail.com>
2021-04-12 10:35:10 -0700

    Added Path check

--- GetSpotlightWallpapers.ps1
+++ GetSpotlightWallpapers.ps1
@@ -1,6 +1,10 @@
-$WallpaperPath = "C:\Users\" + $env:UserName + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 $DesktopPath = "C:\Users\" + $env:UserName + "\Desktop\Wallpapers"
+$WallpaperPath = "C:\Users\" + $env:UserName + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
+
+If( !(test-path $DesktopPath) ){ New-Item -ItemType Directory -Force -Path $DesktopPath }
+If( !(test-path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
 
 cd $DesktopPath
+mkdir $WallpaperPath
 xcopy $WallpaperPath $DesktopPath /i /q /y
 gci -File | Rename-Item -NewName { $_.basename + ".png"}
