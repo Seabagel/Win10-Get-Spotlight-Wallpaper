4d1be3a63c55a8ffead93fe4ab510a2e2430d50c
Seabagel <hsrt1367tim@gmail.com>
2021-04-12 11:14:34 -0700

    Condense copying and renaming into 1 line

--- GetSpotlightWallpapers.ps1
+++ GetSpotlightWallpapers.ps1
@@ -1,14 +1,12 @@
-$DesktopPath = "C:\Users\" + $env:UserName + "\Desktop\Wallpapers"
+$DesktopPath = "C:\Users\" + $env:UserName + "\Desktop\Wallpapers\"
 $WallpaperPath = "C:\Users\" + $env:UserName + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 
 If( !(Test-Path $DesktopPath) ){ New-Item -ItemType Directory -Force -Path $DesktopPath }
 If( !(Test-Path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
 
 cd $DesktopPath
-gci $WallpaperPath -File | 
-foreach {
-  Copy-Item -Destination $DesktopPath
-} 
+gci $WallpaperPath -File | Copy-Item -Force -Destination {$DesktopPath + $_.BaseName + ".png"}
+ 
 # ? { (!Test-Path ($DesktopPath + $BaseName + ".png") }
 # gci $WallpaperPath -File | ? {(-not (Test-Path ($DesktopPath + $BaseName + ".png"))) {Copy-Item -Destination $DesktopPath}}
-gci $DesktopPath -File | Rename-Item -NewName { $_.BaseName + ".png"}
+# gci $DesktopPath -File | Rename-Item -NewName { $_.BaseName + ".png"}
