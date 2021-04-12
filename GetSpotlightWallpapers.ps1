067e4d352aaf2c92905bdf26321fd8ec1f594daf
Seabagel <hsrt1367tim@gmail.com>
2021-04-12 10:40:49 -0700

    Replace xcopy with Copy-Item

--- GetSpotlightWallpapers.ps1
+++ GetSpotlightWallpapers.ps1
@@ -2,9 +2,8 @@ $DesktopPath = "C:\Users\" + $env:UserName + "\Desktop\Wallpapers"
 $WallpaperPath = "C:\Users\" + $env:UserName + "\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 
 If( !(test-path $DesktopPath) ){ New-Item -ItemType Directory -Force -Path $DesktopPath }
-cd $DesktopPath
-
 If( !(test-path $WallpaperPath) ){ New-Item -ItemType Directory -Force -Path $WallpaperPath }
-xcopy $WallpaperPath $DesktopPath /i /q /y
 
-gci -File | Rename-Item -NewName { $_.basename + ".png"}
+cd $DesktopPath
+gci $WallpaperPath -File | Copy-Item -Destination $DesktopPath
+gci $DesktopPath -File | Rename-Item -NewName { $_.basename + ".png"}
